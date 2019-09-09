024d92ebd7eb905d4a60eecfc1c21fc0d108c1ec
andrew dillon <majik895@gmail.com>
2019-09-09 00:37:17 -0400

    GET by id route working

--- api/cars-router.js
+++ api/cars-router.js
@@ -4,18 +4,32 @@ const db = require('../data/db-config')
 
 const router = express.Router();
 
-// GET - render all posts
+/* -------------------------- GET - render all cars ------------------------- */
 router.get('/', async (req, res) => {
     try {
         const cars = await db.find();
-        return res.status(200).json({cars, message: 'Talk about some kick ass cars!'})
-    } catch(error) {
+        return res.status(200).json({ cars, message: 'Talk about some kick ass cars!' })
+    } catch (error) {
         console.log(error);
         res.status(500).json({
             error: "These are not the cars you are looking for."
         });
     }
+}) //Tested and Working
+
+router.get('/:id', async (req, res) => {
+    try {
+        const cars = await db.findById(req.params.id)
+        cars ? res.status(200).json(cars) : res.status(404).json({ error: 'The car with the specified ID does not exist.' })
+    } catch (error) {
+        res.status(500).json({
+            error: 'The car you were looking for could not be retrieved.'
+        })
+    }
 })
+
+
+
 
 
 module.exports = router;
