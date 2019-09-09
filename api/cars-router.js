b9b16f72f98e9d99a1ab21b1d4fb4d4b7895b064
andrew dillon <majik895@gmail.com>
2019-09-09 00:52:01 -0400

    WIP:post new car

--- api/cars-router.js
+++ api/cars-router.js
@@ -17,6 +17,7 @@ router.get('/', async (req, res) => {
     }
 }) //Tested and Working
 
+/* -------------------------- GET render cars by id ------------------------- */
 router.get('/:id', async (req, res) => {
     try {
         const cars = await db.findById(req.params.id)
@@ -26,7 +27,21 @@ router.get('/:id', async (req, res) => {
             error: 'The car you were looking for could not be retrieved.'
         })
     }
-})
+}) //Tested and Working
+
+/* ------------------------ POST add another cool car ----------------------- */
+router.post('/', async (req, res) => {
+    try {
+        const cars = await db.insert(req.body)
+        post ? res.status(201).json(cars) : res.status(400).json({ error: 'Please fill out all required fields before posting a new automobile.' })
+    } catch (error) {
+        res.status(500).json({
+            error: 'There was an error while saving the car to the database.'
+        })
+    }
+}) //Needs Work
+
+
 
 
 
